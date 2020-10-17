08251e8f794f93eebfa6777d296f4e79bf3c85af
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2020-10-17 21:41:46 +0000

    changed the display of the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,43 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0; 
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+     var questions = [
+    'What\'s your favourite color?',
+    'What\'s your favourite animal?',
+  ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first App'),
         ),
-        body: Text('This is my default message'),
+        body: Column(children: [
+          Text(
+            questions[questionIndex],
+          ),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () => print('Answer 2 chosen'),
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () {
+              //......
+              print('Answer 3 chosen!');
+            },
+          ),
+        ]),
       ),
     );
   }

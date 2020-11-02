3cbf7e8997e81ab45275366dc10a325730b230b7
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2020-11-02 07:42:58 +0000

    added styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
-
+import './question.dart';
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0; 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
+    setState(() {
+       _questionIndex = _questionIndex + 1;
+    });
+   
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-     var questions = [
-    'What\'s your favourite color?',
-    'What\'s your favourite animal?',
-  ];
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first App'),
         ),
         body: Column(children: [
-          Text(
-            questions[questionIndex],
+          Question(
+            questions[_questionIndex],
           ),
           RaisedButton(
             child: Text('Answer 1'),
